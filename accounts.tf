5c9b5ef35d800e5ce98c964c0498634c6436d60a
Hatim Khan <bluefireh@gmail.com>
2023-12-06 14:18:02 -0800

    import aws org accounts

--- accounts.tf
+++ accounts.tf
@@ -1,9 +1,19 @@
-resource "aws_organizations_account" "staging" {
-  name = "Staging"
-  email = "staging-hatim@abbey.io"
+import {
+  to = aws_organizations_account.production
+  id = "670657672320"
 }
 
-resource "aws_organizations_account" "production" {
-  name = "Production"
-  email = "production-hatim@abbey.io"
+import {
+  to = aws_organizations_account.staging
+  id = "239750427433"
 }
+
+# import "aws_organizations_account" "staging" {
+#   name = "Staging"
+#   email = "staging-hatim@abbey.io"
+# }
+
+# resource "aws_organizations_account" "production" {
+#   name = "Production"
+#   email = "production-hatim@abbey.io"
+# }
