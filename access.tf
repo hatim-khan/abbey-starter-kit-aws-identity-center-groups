93443579060f8ac5505427aaca49b9ddbfb2428e
Abbey <no-reply@abbey.io>
2023-12-06 22:32:02 +0000

    hat@abbey.io requests access to: group_membership_grant

---
+++ access.tf
@@ -0,0 +1,5 @@
+resource "aws_identitystore_group_membership" "user_14d894f8-6031-705f-904b-9aeb4a46404e_group_1438f498-f021-70af-065f-76548922aa1b" {
+  member_id = "14d894f8-6031-705f-904b-9aeb4a46404e"
+  group_id = "1438f498-f021-70af-065f-76548922aa1b"
+  identity_store_id = "d-90679e5f8a"
+}
